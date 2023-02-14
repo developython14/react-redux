0338edd66d6d05ce812a2ac2cf525a3a2e54896c
developython14 <djmel1955@gmail.com>
2023-02-14 04:13:13 +0100

    add the requirements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Counter } from './componanat/counter';
+import Comment from './componanat/test';
 function App() {
   return (
     <div className="App">
    <Counter/>
+   <Comment/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { mystore } from './provider/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={mystore}>
