565ce1a4e52818ec5e97c8ea4b12686570c99299
developython14 <djmel1955@gmail.com>
2023-02-19 16:32:39 +0100

    add the requirements

--- src/componanat/Test_Camera.js
+++ src/componanat/Test_Camera.js
@@ -7,6 +7,11 @@ function Test_devices() {
     
     return <div>
         <My_nav/>
+        <div className='d-flex flex-row'>
+            <div className='video-bar'>
+            <video id="localVideo" autoplay playsinline controls="false"/>
+            </div>
+        </div>
     </div>
 }
 
