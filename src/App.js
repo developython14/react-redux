5e22993cf7038fc25089d6efb302bd301ff82c4a
developython14 <djmel1955@gmail.com>
2023-02-22 04:27:12 +0100

    add the requirements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import { Counter } from './componanat/counter';
 import Test_devices from './componanat/Test_Camera';
-import { ReactMediaRecorder } from "react-media-recorder";
+import { useReactMediaRecorder  } from "react-media-recorder";
 
-const RecordView = () => (
-  <div>
-    <ReactMediaRecorder
-      video
-      screen
-      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-        <div>
-          <p>{status}</p>
-          <button onClick={startRecording}>Start Recording</button>
-          <button onClick={stopRecording}>Stop Recording</button>
-          <video src={mediaBlobUrl} controls autoPlay loop />
-        </div>
-      )}
-    />
-  </div>
-);
+const RecordView = () => {
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ video: true  , facingMode: { exact: "environment" }});
+
+  return (
+    <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
