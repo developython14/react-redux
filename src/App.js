46380e7cdd964c5ba25df4103e948f5573592799
developython14 <djmel1955@gmail.com>
2023-02-22 05:58:07 +0100

    COMPLETE testing devices tasks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import './App.css';
 import { Counter } from './componanat/counter';
 import Test_devices from './componanat/Test_Camera';
-import { useReactMediaRecorder  } from "react-media-recorder";
-const RecordView = () => {
-  const { status, startRecording, stopRecording, mediaBlobUrl } =
-    useReactMediaRecorder({ video: true  });
 
-  return (
-    <div>
-      <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <video src={mediaBlobUrl} controls autoPlay loop />
-    </div>
-  );
-};
 
 function App() {
   return (
--- src/componanat/Test_Camera.js
+++ src/componanat/Test_Camera.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import { Navbar , NavbarBrand ,FormGroup,Label,Input,Col ,Form} from 'reactstrap';
 import My_nav from './nav';
+import { useReactMediaRecorder  } from "react-media-recorder";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+
 
 
 
 
 
 function Test_devices() {
+    const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ video: true  });
     const startVideo = async() => {
         const constraints = {'video': true, 'audio': false};
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -80,11 +86,51 @@ function Test_devices() {
                 </Col>
             </FormGroup>
             <a href="#" className="myButton">Join Room</a>
-            <a href="#" className="myButton1">Test devices</a>
+            <Devices_Tester/>
             </div>
         </div>
     </div>
 }
+
+
+
+function Devices_Tester(args) {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ video: true  });
+  return (
+    <div>
+      <Button color="danger" className="myButton1" onClick={toggle}>
+      Test devices
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} {...args} fullscreen>
+        <ModalHeader toggle={toggle}>Test Devices</ModalHeader>
+        <ModalBody>
+        <div>
+      <p>{status}</p>
+      
+      <video src={mediaBlobUrl} controls autoPlay loop width={'50%'} height = {"50%"} />
+    </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={startRecording}>
+          Start Recording
+          </Button>
+          <Button color="danger" onClick={stopRecording}>
+          Stop Recording
+          </Button>
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+
 
 
 
